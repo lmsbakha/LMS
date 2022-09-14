0da51ff2670762ab872d49d4feb694d9852769cb
s_s12 <s_s12@DESKTOP-FG768SV>
2022-09-14 11:06:21 +0900

    [박찬웅] 부트스트랩 초기 설정

---
+++ lms/src/test/java/com/gd/test0907/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.test0907;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
