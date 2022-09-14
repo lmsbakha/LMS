359d35c6ee8992fc7178b3ded3f23879a37cdac4
GDJ50 <GDJ50@DESKTOP-FEU7CVT>
2022-09-14 17:22:40 +0900

    [박소정] DB연동 테스트

---
+++ lms/src/main/java/com/gd/lms/commons/EncodingFilter.java
@@ -0,0 +1,26 @@
+package com.gd.lms.commons;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j // Simple Logging Facade for Java, 로깅에 대한 인터페이스 모음
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+      
+      request.setCharacterEncoding("utf-8");
+      // 디버깅
+      log.debug(TeamColor.debuging + "===EncodingFilter utf-8 인코딩 실행===" +TeamColor.TEXT_RESET);
+      // pass the request along the filter chain
+      chain.doFilter(request, response);
+   }
+
+}
--- lms/src/main/java/com/gd/lms/service/AccountService.java
+++ lms/src/main/java/com/gd/lms/service/AccountService.java
@@ -1,12 +1,27 @@
 package com.gd.lms.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.gd.lms.commons.TeamColor;
+import com.gd.lms.mapper.AccountMapper;
+import com.gd.lms.vo.Account;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Transactional
 @Service
 public class AccountService {
-
+	@Autowired AccountMapper accountMapper;
+	
+	//test
+	public Account getAccount() {
+		//리턴
+		Account account = accountMapper.selectAccount();
+		System.out.println(account+"<-- account");
+		log.debug(TeamColor.PSJ + account +TeamColor.TEXT_RESET);
+		return account;
+	};
 }
