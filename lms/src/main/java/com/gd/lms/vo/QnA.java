289c840193ab7983cc41c9fd1d3e83ec96a05df5
WhiteHar <hundredsky@naver.com>
2022-09-14 17:08:04 +0900

    [이하늘] vo/table 재확인

--- lms/src/main/java/com/gd/lms/vo/QnA.java
+++ lms/src/main/java/com/gd/lms/vo/QnA.java
@@ -8,7 +8,7 @@ public class QnA {
 	private String qnaTitle;
 	private String qnaContent;
 	private String accountId;
-	private String CreateDate;
+	private String createDate;
 	private String qnaDisclosure;	// 공개 or 비공개
 	private String qnaKind;			// 질문 or 답변
 	private String qnaState;		// 대기중 or 답변완료
