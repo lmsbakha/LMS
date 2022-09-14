04624fe1770f764cfd7b3ed1b987f26765a364a8
GDJ50 <GDJ50@DESKTOP-FEU7CVT>
2022-09-14 14:55:01 +0900

    [박소정] 팀 컬러 초기 설정

---
+++ lms/src/main/java/com/gd/lms/commons/TeamColor.java
@@ -0,0 +1,10 @@
+package com.gd.lms.commons;
+
+public class TeamColor {
+	public static final String TEXT_RESET = "\u001B[0m";
+	public final static String PCW = "\u001B[31m";	//빨강
+	public final static String PSJ = "\u001B[32m";	//초록
+	public final static String PSY = "\u001B[33m";	//노랑
+	public final static String LHN = "\u001B[35m";	//보라
+}
+
