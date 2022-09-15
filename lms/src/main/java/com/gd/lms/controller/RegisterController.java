d670fbfa1cda559825877a85a0f5947c476fc775
GDJ50 <GDJ50@DESKTOP-FEU7CVT>
2022-09-15 17:27:51 +0900

    [박소정] register.jsp 구성(다음 주소 API 사용), registerController

--- lms/src/main/java/com/gd/lms/controller/RegisterController.java
+++ lms/src/main/java/com/gd/lms/controller/RegisterController.java
@@ -1,8 +1,17 @@
 package com.gd.lms.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RegisterController {
-
+	// register.jsp 접속시 나타나는 회원가입 폼
+	// 파라미터 : X
+	// 리턴값 : 회원가입 폼
+	@GetMapping("/register")
+	public String login(HttpSession session) {
+		return "register";
+	}
 }
