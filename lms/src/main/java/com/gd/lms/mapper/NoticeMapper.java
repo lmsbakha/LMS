ca81a84196df129bd8fcaf29a0559aba2ea2502e
WhiteHar <hundredsky@naver.com>
2022-09-15 17:50:56 +0900

    [이하늘] noticeList, addNotice 작업중

--- lms/src/main/java/com/gd/lms/mapper/NoticeMapper.java
+++ lms/src/main/java/com/gd/lms/mapper/NoticeMapper.java
@@ -1,8 +1,18 @@
 package com.gd.lms.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.gd.lms.vo.Notice;
 
 @Mapper
 public interface NoticeMapper {
-
+	// 공지사항 리스트
+	List<Map<String, Object>> selectNoticeList();
+	// 공지사항 작성 액션
+	int addNotice(Notice notice);
+	// 수정
+	// 삭제
 }
--- lms/src/main/java/com/gd/lms/service/NoticeService.java
+++ lms/src/main/java/com/gd/lms/service/NoticeService.java
@@ -1,12 +1,37 @@
 package com.gd.lms.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.gd.lms.commons.TeamColor;
+import com.gd.lms.mapper.NoticeMapper;
+import com.gd.lms.vo.Notice;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Transactional
 @Service
 public class NoticeService {
-
+	@Autowired private NoticeMapper noticeMapper;
+	
+	// 공지사항 리스트
+	public List<Map<String, Object>> getNoticeList(){
+		List<Map<String, Object>> noticeList = noticeMapper.selectNoticeList();
+		return noticeList;
+	}
+	
+	// 공지사항 신규 작성
+	public int addNotice(Notice notice) {
+		// 디버깅
+		log.debug(TeamColor.debuging+" addNotice 실행" + TeamColor.LHN + TeamColor.TEXT_RESET);
+		int row = noticeMapper.addNotice(notice);
+		return row;
+	}
+	
+	
 }
