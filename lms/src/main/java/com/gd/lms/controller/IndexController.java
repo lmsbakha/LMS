4ab2c360ac91f0a075244406609ba7fa44d6b78a
GDJ50 <GDJ50@DESKTOP-FEU7CVT>
2022-09-15 12:21:33 +0900

    [박소정] 로그인/로그아웃 기능 구현(Controller/Service/Mapper)

--- lms/src/main/java/com/gd/lms/controller/IndexController.java
+++ lms/src/main/java/com/gd/lms/controller/IndexController.java
@@ -1,14 +1,10 @@
 package com.gd.lms.controller;
 
-import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.gd.lms.service.AccountService;
-import com.gd.lms.vo.Account;
 
 @Controller
 public class IndexController {
@@ -19,13 +15,4 @@ public class IndexController {
 		return "index";
 	}
 
-	@GetMapping("/test")
-   public String test(HttpSession session) {
-	   //서비스 콜하기
-		String testId = "admin";
-	   Account account = accountService.getAccount(testId);
-	   System.out.println(account+"<-- account");
-	   session.setAttribute("account", account);
-	   return "test";	//포워딩
-   }
 }
--- lms/src/main/java/com/gd/lms/mapper/AccountMapper.java
+++ lms/src/main/java/com/gd/lms/mapper/AccountMapper.java
@@ -6,5 +6,8 @@ import com.gd.lms.vo.Account;
 
 @Mapper
 public interface AccountMapper {
-	Account selectAccount(String testId);
+	// 로그인에 사용되는 메소드
+	// 파라미터 : account (accountId, accountPw)
+	// 리턴값 : account (accountId, accountLevel, accountState)
+	Account selectLogin(Account account);
 }
