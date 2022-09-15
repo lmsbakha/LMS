fd75df1ef07054ef37157463f09bfb162aa1e6de
GDJ50 <GDJ50@DESKTOP-FEU7CVT>
2022-09-15 19:44:54 +0900

    [박소정,박소연,박찬웅] accountLevel에 따라 다른 계정권한으로 가입되도록 회원가입 구현 완료/회원가입 성공 후 로그인 페이지로 리다이렉트 성공 확인했습니다

--- lms/src/main/java/com/gd/lms/controller/RegisterController.java
+++ lms/src/main/java/com/gd/lms/controller/RegisterController.java
@@ -1,20 +1,70 @@
 package com.gd.lms.controller;
 
-import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.gd.lms.commons.TeamColor;
+import com.gd.lms.service.MemberService;
+import com.gd.lms.vo.Member;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class RegisterController {
+	//MemberService 객체 주입
+	@Autowired private MemberService memberService;
+	
 	// register.jsp 접속시 나타나는 회원가입 폼
 	// 파라미터 : X
 	// 리턴값 : 회원가입 폼
 	@GetMapping("/register")
-	public String login(HttpSession session) {
+	public String register() {
 		return "register";
 	}
-	
-	// register.jsp
-	
+
+	// register.jsp에서 받아온 회원정보를 이용해서 회원가입하는 액션 메소드
+	// 파라미터 : member
+	// 리턴값 : 로그인 폼으로 이동하기
+	@PostMapping("/register")
+	public String register(@RequestParam(value = "accountId") String accountId,
+			@RequestParam(value = "memberName") String memberName,
+			@RequestParam(value = "memberGender") String memberGender,
+			@RequestParam(value = "memberBirth") String memberBirth,
+			@RequestParam(value = "memberEmail") String memberEmail,
+			@RequestParam(value = "memberPhone") String memberPhone,
+			@RequestParam(value = "memberAddress") String memberAddress,
+			@RequestParam(value = "memberDetailAddress") String memberDetailAddress,
+			@RequestParam(value = "accountLevel") int accountLevel) {
+		// 요청받은 값 Map 객체에 셋팅
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("accountId", accountId);
+		paramMap.put("memberName", memberName);
+		paramMap.put("memberGender", memberGender);
+		paramMap.put("memberBirth", memberBirth);
+		paramMap.put("memberEmail", memberEmail);
+		paramMap.put("memberPhone", memberPhone);
+		paramMap.put("memberAddress", memberAddress);
+		paramMap.put("memberDetailAddress", memberDetailAddress);
+		paramMap.put("accountLevel", accountLevel);
+		// 디버깅
+		log.debug(TeamColor.PSY + paramMap + "<-- paramMap" + TeamColor.TEXT_RESET);
+		// service call
+		int row = memberService.addMember(paramMap);
+
+		if (row != 0) { // 성공
+			// 파라미터 디버깅
+			log.debug(TeamColor.PSY + " 회원가입 성공" + TeamColor.TEXT_RESET);
+			return "redirect:/login";
+		} else { // 실패
+			log.debug(TeamColor.PSY + " 회원가입 실패" + TeamColor.TEXT_RESET);
+			return "redirect:/register";
+		}
+	}
 }
--- lms/src/main/java/com/gd/lms/service/MemberService.java
+++ lms/src/main/java/com/gd/lms/service/MemberService.java
@@ -1,11 +1,67 @@
 package com.gd.lms.service;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gd.lms.commons.TeamColor;
+import com.gd.lms.mapper.AccountMapper;
+import com.gd.lms.mapper.MemberMapper;
+import com.gd.lms.vo.Account;
+import com.gd.lms.vo.Member;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Transactional
 @Service
 public class MemberService {
+	// MemberMapper 객체 주입
+	@Autowired
+	private MemberMapper memberMapper;
+	// AccountMapper 객체 주입
+	@Autowired
+	private AccountMapper accountMapper;
+
+	public int addMember(Map<String, Object> paramMap) {
+		// 파라미터 디버깅
+		log.debug(TeamColor.PSJ + paramMap + "<--paramMap" + TeamColor.TEXT_RESET);
+
+		// account 객체에 데이터 셋팅
+		Account paramAccount = new Account();
+		paramAccount.setAccountId((String) paramMap.get("accountId"));
+		paramAccount.setAccountLevel((int) paramMap.get("accountLevel"));
+		// 디버깅
+		log.debug(TeamColor.PSJ + paramAccount + "<--paramAccount" + TeamColor.TEXT_RESET);
+
+		// Member 객체에 데이터 셋팅
+		Member paramMember = new Member();
+		paramMember.setAccountId((String) paramMap.get("accountId"));
+		paramMember.setMemberName((String) paramMap.get("memberName"));
+		paramMember.setMemberGender((String) paramMap.get("memberGender"));
+		paramMember.setMemberBirth((String) paramMap.get("memberBirth"));
+		paramMember.setMemberEmail((String) paramMap.get("memberEmail"));
+		paramMember.setMemberPhone((String) paramMap.get("memberPhone"));
+		paramMember.setMemberAddress((String) paramMap.get("memberAddress"));
+		paramMember.setMemberDetailAddress((String) paramMap.get("memberDetailAddress"));
+
+		// Mapper call
+		// Account table에 먼저 추가 --> Member table에 추가
+		int addAccount = accountMapper.insertAccount(paramAccount);
+		int addMember = 0;
+		if (addAccount != 0) {
+			log.debug(TeamColor.PSJ + "account 테이블에 회원 추가 성공" + TeamColor.TEXT_RESET);
+			addMember = memberMapper.insertMember(paramMember);
+			if (addMember != 0) {
+				log.debug(TeamColor.PSJ + "Member 테이블에 회원 추가 성공" + TeamColor.TEXT_RESET);
+			} else {
+				log.debug(TeamColor.PSJ + "Member 테이블에 회원 추가 실패" + TeamColor.TEXT_RESET);
+			}
+		} else {
+			log.debug(TeamColor.PSJ + "account 테이블에 회원 추가 실패" + TeamColor.TEXT_RESET);
+		}
+		return addMember;
+	}
 }
