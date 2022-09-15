b594acfc803a595bcba91ade019cd29aec9c19b2
GDJ50 <GDJ50@DESKTOP-FEU7CVT>
2022-09-15 09:08:18 +0900

    [박소정] LoginController 클래스 생성

--- lms/src/main/java/com/gd/lms/commons/EncodingFilter.java
+++ lms/src/main/java/com/gd/lms/commons/EncodingFilter.java
@@ -14,13 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j // Simple Logging Facade for Java, 로깅에 대한 인터페이스 모음
 @WebFilter("/*")
 public class EncodingFilter extends HttpFilter implements Filter {
-   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-      
-      request.setCharacterEncoding("utf-8");
-      // 디버깅
-      log.debug(TeamColor.debuging + "===EncodingFilter utf-8 인코딩 실행===" +TeamColor.TEXT_RESET);
-      // pass the request along the filter chain
-      chain.doFilter(request, response);
-   }
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// 인코딩
+		request.setCharacterEncoding("utf-8");
+		// 디버깅
+		log.debug(TeamColor.debuging + "===EncodingFilter utf-8 인코딩 실행===" + TeamColor.TEXT_RESET);
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
 
 }
