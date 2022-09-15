13379e4d22aa41d22a7219147dab5c56a1a93188
WhiteHar <hundredsky@naver.com>
2022-09-15 12:55:06 +0900

    [이하늘] register 진행중

--- lms/src/main/java/com/gd/lms/mapper/MemberMapper.java
+++ lms/src/main/java/com/gd/lms/mapper/MemberMapper.java
@@ -1,7 +1,13 @@
 package com.gd.lms.mapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import com.gd.lms.vo.Member;
+
 @Mapper
 public interface MemberMapper {
-
+	// 회원가입 시 계정 생성
+		Member insertMember(Member member);
+		
+	// 회원가입 시 아이디 중복검사
+	//	boolean IdCheck(Member member);
 }
