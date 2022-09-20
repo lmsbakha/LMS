a9a9b426555e503500c7d0c4703c390241b5d123
ssoyyeon <110171449+ssoyyeon@users.noreply.github.com>
2022-09-20 15:18:12 +0900

    [박소연] lecture Mapper 삭제

--- lms/src/main/java/com/gd/lms/mapper/LectureMapper.java
+++ lms/src/main/java/com/gd/lms/mapper/LectureMapper.java
@@ -1,15 +1,1 @@
 package com.gd.lms.mapper;
-
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-
-import com.gd.lms.vo.Lecture;
-
-@Mapper
-public interface LectureMapper {
-	// 강의 정보 조회하기
-	// 파라미터 : X
-	// 리턴값 : List<Lecture>
-	List<Lecture> selectLectureList();
-}
