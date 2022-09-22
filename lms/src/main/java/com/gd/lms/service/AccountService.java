4d905ebb21a0641b73c8f7fcbc4b1b00f36f56ee
s_s12 <s_s12@PCW>
2022-09-23 08:49:43 +0900

    [박찬웅] 계정 활성화 상태값 '휴면'인 사용자 '활성화'로 업데이트하는 기능 / 마지막 로그인날짜가 90이상 지나면 자동으로 스프링스케줄러를 통해 활성화값을 '활성화'를 '휴면으로' 업데이트하는 기능 구현

--- lms/src/main/java/com/gd/lms/service/AccountService.java
+++ lms/src/main/java/com/gd/lms/service/AccountService.java
@@ -19,21 +19,40 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class AccountService {
 	// AccountMapper 객체 주입
-	@Autowired AccountMapper accountMapper;
-	
+	@Autowired
+	AccountMapper accountMapper;
+
 	// accountState 활성화값
 	public String getAccountState(Account account) {
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService getAccountState" +TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService getAccountState account : " + account + TeamColor.TEXT_RESET);
+
 		return accountMapper.selectAccountState(account);
+	}
+
+	// accountState 활성화값 수정 - '휴면' -> '활성화'
+	public int modifyAccoutStateByMember(Account account) {
+		// 디버깅
+		log.debug(TeamColor.PCW + "AccountService getAccountState account : " + account + TeamColor.TEXT_RESET);
+		
+		int row = accountMapper.updateAccountStateByMember(account);
+		
+		return row;
+	}
+	
+	// 계정 마지막 로그인날짜로부터 90일동안 로그인 안했다면 활성화 상태값 수정 - '활성화' -> '휴면'
+	public int modifyAccountStateByMemberLastLogin() {
+		// 디버깅
+		log.debug(TeamColor.PCW + "AccountService modifyAccountStateByMemberLastLogin " + TeamColor.TEXT_RESET);
+		
+		return accountMapper.updateAccountStateByMemberLastLogin();
 	}
 	
 	// 로그인
 	public Account getLogin(Account paramAccount) {
 		// 디버깅
 		log.debug(TeamColor.PCW + "AccountService getLogin paramAccount" + paramAccount + TeamColor.TEXT_RESET);
-		
+
 		// 로그인 정보 대입해서 맞다면 로그인 아이디와 해당 level, state 가지고오기
 		Account account = accountMapper.selectLogin(paramAccount);
 		// 디버깅
@@ -42,133 +61,124 @@ public class AccountService {
 		int row = accountMapper.updateLastLoginDate(paramAccount.getAccountId());
 		// 디버깅
 		log.debug(TeamColor.PCW + "AccountService getLogin row" + row + TeamColor.TEXT_RESET);
-		
+
 		return account;
 	}
-	
+
 	// (학생, 강사, 행정) 멤버 아이디 찾기
 	public String searchMemberAccountId(Map<String, Object> map) {
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService searchMemberAccountId "+ map + TeamColor.TEXT_RESET);
+		log.debug(TeamColor.PCW + "AccountService searchMemberAccountId " + map + TeamColor.TEXT_RESET);
 		String accountId = accountMapper.selectMemberAccountId(map);
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService searchMemberAccountId accountId "+ accountId + TeamColor.TEXT_RESET);
-		
-		if(accountId == null) {
+		log.debug(TeamColor.PCW + "AccountService searchMemberAccountId accountId " + accountId + TeamColor.TEXT_RESET);
+
+		if (accountId == null) {
 			return "false";
-		} 
-		
+		}
+
 		return accountId;
 	}
-	
+
 	// (학생, 강사, 행정) 멤버 비밀번호 찾기
 	public int searchMemberAccountPw(Map<String, Object> map) {
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService searchMemberAccountPw map : "+ map + TeamColor.TEXT_RESET);
+		log.debug(TeamColor.PCW + "AccountService searchMemberAccountPw map : " + map + TeamColor.TEXT_RESET);
 		int cnt = accountMapper.selectMemberAccountPw(map);
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService searchMemberAccountPw cnt : "+ cnt + TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService searchMemberAccountPw cnt : " + cnt + TeamColor.TEXT_RESET);
+
 		return cnt;
 	}
-	
+
 	// (학생, 강사, 행정) 멤버 비밀번호 변경
 	public int modifySearchMemberAccountPw(Account account) {
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService modifySearchMemberAccountPw "+ account + TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService modifySearchMemberAccountPw " + account + TeamColor.TEXT_RESET);
+
 		return accountMapper.updateSearchMemberAccountPw(account);
 	}
-	
+
 	// 회원가입
 	public void addMember(Member paramMember) {
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService addMember" +TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService addMember" + TeamColor.TEXT_RESET);
+
 		// 레벨 변수 선언
 		String level = "";
-		if("manager".equals(paramMember.getMemberCheck())) { // memberCheck 매니저인지
+		
+		if ("manager".equals(paramMember.getMemberCheck())) { // memberCheck 매니저인지
 			level = "3";
-		} else if("teacher".equals(paramMember.getMemberCheck())) { // memberCheck 강사인지
+		} else if ("teacher".equals(paramMember.getMemberCheck())) { // memberCheck 강사인지
 			level = "2";
 		} else { // memberCheck 학생
 			level = "1";
 		}
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService addMember level :" + level +TeamColor.TEXT_RESET);
+		log.debug(TeamColor.PCW + "AccountService addMember level :" + level + TeamColor.TEXT_RESET);
 		// Member level 부여
 		paramMember.setAccountLevel(level);
-		
+
 		// account 테이블 insert
 		accountMapper.insertAccount(paramMember);
 		// member 테이블 insert
 		accountMapper.insertMember(paramMember);
 	}
-	
+
 	// 아이디 중복체크
 	public int accountIdCheck(String idck) {
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService accountIdCheck" +TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService accountIdCheck" + TeamColor.TEXT_RESET);
+
 		// 해당 아이디 있는지 값 체크하고 일치한다면 Cnt 받아오기
 		int cnt = accountMapper.selectAccountIdCnt(idck);
 		// 디버깅
 		log.debug(TeamColor.PCW + "AccountService accountIdCheck cnt : " + cnt + TeamColor.TEXT_RESET);
-		
+
 		return cnt;
-		
+
 	}
-	
+
 	// 이메일 중복체크 - student
 	public int studentEmailCheck(String email) {
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService memberEmailCheck - student" +TeamColor.TEXT_RESET);
-		log.debug(TeamColor.PCW + "AccountService memberEmailCheck email : "+ email +TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService memberEmailCheck - student" + TeamColor.TEXT_RESET);
+		log.debug(TeamColor.PCW + "AccountService memberEmailCheck email : " + email + TeamColor.TEXT_RESET);
+
 		// 해당 이메일 있는지 값 체크하고 일치한다면 Cnt 받아오기
 		int cnt = accountMapper.selectStudentEmailCnt(email);
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService studentEmailCheck cnt : "+ cnt + TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService studentEmailCheck cnt : " + cnt + TeamColor.TEXT_RESET);
+
 		return cnt;
 	}
-	
+
 	// 이메일 중복체크 - teacher
 	public int teacherEmailCheck(String email) {
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService memberEmailCheck - teacher" +TeamColor.TEXT_RESET);
-		log.debug(TeamColor.PCW + "AccountService memberEmailCheck email : "+ email +TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService memberEmailCheck - teacher" + TeamColor.TEXT_RESET);
+		log.debug(TeamColor.PCW + "AccountService memberEmailCheck email : " + email + TeamColor.TEXT_RESET);
+
 		// 해당 이메일 있는지 값 체크하고 일치한다면 Cnt 받아오기
 		int cnt = accountMapper.selectTeacherEmailCnt(email);
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService teacherEmailCheck cnt : "+ cnt + TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService teacherEmailCheck cnt : " + cnt + TeamColor.TEXT_RESET);
+
 		return cnt;
 	}
-	
+
 	// 이메일 중복체크 - manager
 	public int managerEmailCheck(String email) {
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService memberEmailCheck -manager" +TeamColor.TEXT_RESET);
-		log.debug(TeamColor.PCW + "AccountService memberEmailCheck email : "+ email +TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService memberEmailCheck -manager" + TeamColor.TEXT_RESET);
+		log.debug(TeamColor.PCW + "AccountService memberEmailCheck email : " + email + TeamColor.TEXT_RESET);
+
 		// 해당 이메일 있는지 값 체크하고 일치한다면 Cnt 받아오기
 		int cnt = accountMapper.selectManagerEmailCnt(email);
 		// 디버깅
-		log.debug(TeamColor.PCW + "AccountService managerEmailCheck cnt : "+ cnt + TeamColor.TEXT_RESET);
-		
+		log.debug(TeamColor.PCW + "AccountService managerEmailCheck cnt : " + cnt + TeamColor.TEXT_RESET);
+
 		return cnt;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
